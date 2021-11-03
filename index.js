234b05951252901bd9445120023789f0eb842094
Nar Bhujel <80000799+nar186@users.noreply.github.com>
2021-11-03 14:34:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log(document);
 const heading = document.querySelector("h1");
 console.log(heading);
-const value = document.querySelector("value");
-console.log(value);
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
